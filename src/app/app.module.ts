0aa144d8da4ffb0ac989817dcb1b08b932757010
Krikunova Olga <shersh.ol@gmail.com>
2022-06-18 13:51:57 +0700

    added sctructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonContainerComponent } from './container/person-container/person-container.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonContainerComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/persons/container/person-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-container',
+  templateUrl: './person-container.component.html',
+  styleUrls: ['./person-container.component.scss']
+})
+export class PersonContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
